3ae48179fe446b6a391eabbc69e3033b2a011493
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-11-06 10:17:34 +0500

    Initial commit

---
+++ src/main/java/com/example/inventorysystemwithgpt/InventorySystemWithGptApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventorysystemwithgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySystemWithGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventorySystemWithGptApplication.class, args);
+    }
+
+}
